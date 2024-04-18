d0a2028353d353c21ea3d810e5f2e045340a8ef1
mvtvn78 <mvtvn78@gmail.com>
2024-04-18 13:15:54 +0700

    List datastructure

---
+++ list.dart
@@ -0,0 +1,12 @@
+// 2 Syntax to declare for array
+List<int> numbers = [];
+var arr =[];
+void main() {
+  numbers.add(5);
+  numbers.add(3);
+  numbers.add(100);
+  numbers.forEach( (i) {
+    print(i);
+  });
+}
+
