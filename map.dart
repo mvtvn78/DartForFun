653ade256e08b3b3fef1ed8cba8a95f4f20470db
mvtvn78 <mvtvn78@gmail.com>
2024-04-18 13:25:15 +0700

    MAP Datastructure

---
+++ map.dart
@@ -0,0 +1,13 @@
+var map = {
+  "author" : "Mai Van Tien",
+  "age": 20,
+  "mailcontact" : "maivantien594dz@gmail.com"
+};
+
+void main() {
+  
+  map.forEach( (key,value) {
+    print( ---- );
+  });
+}
+
