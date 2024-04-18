f114bd2bec6aae2ae8410a4c42cfab25bf9bf5ec
mvtvn78 <mvtvn78@gmail.com>
2024-04-18 12:08:11 +0700

    Different between dynamic and var in dart

---
+++ dynamicandvar.dart
@@ -0,0 +1,26 @@
+void main() {
+	dynamic a;
+	print(a.runtimeType);
+	print(a);
+
+	a=5;
+	print(a.runtimeType);
+	print(a);
+
+	var b;
+	print(b.runtimeType);
+
+	b=10;
+	print(b.runtimeType);
+	print(b);
+	
+	b= "String";
+	print(b.runtimeType);
+	print(b);
+
+	var c = 5;
+	c= 10;
+	print(c);
+	c= "mvtvn78";
+	print(c);	
+}
---
+++ variables.dart
@@ -0,0 +1,8 @@
+// Syntax : typedate  name_var = value;
+void main() {
+	String author = "mvtvn78";
+	// var automatically detect datatype in value;
+	var title = "Helloworld";
+	print(author);
+	print(titile);
+}
